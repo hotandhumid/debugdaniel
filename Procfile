e65ae938124a3bc5c637bb9b3d25319a6ab39342
hotandhumid <jack.jiaen.he@gmail.com>
2023-07-21 11:58:06 -0400

    first commit

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn CSS.wsgi
--- playground/skills_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('python_projects/', views.python_proj),
-    path('html_projects/', views.html_proj),
-    path('cpp_projects/', views.cpp_proj),
-]
